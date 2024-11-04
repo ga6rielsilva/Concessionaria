7252e07b30b74e30a1ce9b1bf843b220b87c4822
Welly <wellysonrudnick13@gmail.com>
2024-11-04 17:29:08 -0300

    04-11-24

---
+++ Concessionaria/app.py
@@ -0,0 +1,12 @@
+# app.py
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    user = "Maria"
+    return render_template('index.html', user=user)
+
+if __name__ == '__main__':
+    app.run(debug=True)
