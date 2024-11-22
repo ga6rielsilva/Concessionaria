1b21d24c6addd22b1c7996b2cf76a8211a3531ac
ga6rielsilva <ga6rielsilva@gmail.com>
2024-11-22 15:30:28 -0300

    feat: Update database structure and tables

---
+++ Banco de dados/bd-connection.py
@@ -0,0 +1,12 @@
+import mysql.connector
+
+# Estabelecer conexão com o banco de dados
+conn = mysql.connector.connect(
+    host='localhost',
+    user='root',
+    password='',
+    database='erp_concessionaria'
+)
+
+# Criar um cursor para manipular o banco de dados
+cursor = conn.cursor()
